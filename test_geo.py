ad8ac6b33e22ff930af9fcd5d9c3670139b16edf
Lorcan Nicholls <ln356@cam.ac.uk>
2021-01-23 22:22:27 +0000

    Added Unit Test for Task 1B

---
+++ test_geo.py
@@ -0,0 +1,24 @@
+'''
+Unit test for the geo module.
+'''
+
+from floodsystem.geo import stations_by_distance
+from floodsystem.stationdata import build_station_list
+import functools
+
+def test_stations_by_distance():
+
+    CAMBRIDGE_CITY_CENTRE = (52.2053, 0.1218)
+    stations_distances = stations_by_distance(build_station_list(), CAMBRIDGE_CITY_CENTRE)
+    num_stations = len(stations_distances)
+
+    # Ensure the stations list is not empty
+    assert num_stations > 0
+
+    # Ensure each station's distance is geq than the previous one
+    # (in increasing order). The >= is required since sometimes two 
+    # of the same stations are listed, so gives equal distances.
+    assert all([stations_distances[i+1][1] >= 
+                stations_distances[i][1] for i in range(num_stations - 1)])
+
+test_stations_by_distance()
