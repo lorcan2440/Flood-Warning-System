efa2f33b681e7fcc3f05a843705d0b4b788d38ba
Lorcan Nicholls <ln356@cam.ac.uk>
2021-09-05 14:31:55 +0100

    Added info to map viewer text output

--- MapViewer.py
+++ MapViewer.py
@@ -7,6 +7,10 @@ def run():
     stations = build_station_list()
     update_water_levels(stations)
     display_stations_on_map(stations)
+    print(f'Displaying {len(stations)} monitoring stations on the map. \n'
+    'Use the toolbar on the right to interact with the map e.g. zoom, pan, save. \n'
+    'Hover over a dot to view basic info about the station. \n'
+    'Click on a dot to view the graph of the level data on the official gov.uk site.')
 
 
 if __name__ == "__main__":
