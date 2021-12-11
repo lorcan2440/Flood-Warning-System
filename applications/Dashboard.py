1a181d895932d352f9eae9c2aae6efbce95934ab
Lorcan Nicholls <ln356@cam.ac.uk>
2021-12-11 14:48:27 +0000

    First dashboard created

--- applications/Dashboard.py
+++ applications/Dashboard.py
@@ -16,13 +16,15 @@ def run():
 if __name__ == '__main__':
 
     if len(sys.argv) > 1:
-
-        TERMINATE_APP_TIME = float(sys.argv[1].strip())
+        args = sys.argv[1:]
+        TERMINATE_APP_TIME = float(args[0].strip())
         t1 = multiprocessing.Process(target=run)
         t1.start()
         time.sleep(TERMINATE_APP_TIME)
+
+        # NOTE: use a shutdown request instead of a forceful closure:
+        # https://community.plotly.com/t/shutdown-dash-app-after-n-seconds/20422
         print(f'Terminating process because script called with argument {TERMINATE_APP_TIME} seconds...')
         t1.terminate()
-
     else:
         run()
